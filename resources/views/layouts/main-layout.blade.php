0836fc21a10d52d2b40b28a1793693c4eab37192
supeerrookie <supeerrookie@users.noreply.github.com>
2019-11-13 15:32:20 +0700

    updated lastest meta description
    also fix bugs scroll on firefox mobile

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -8,13 +8,13 @@
 <head>
     <title>Wave of Tomorrow 2019 | Art, Techology & Music Experience AHEAD of its Time</title>
     <meta name="title" content="Wave of Tomorrow Indonesia  | Art, Techology & Music Experience AHEAD of its Time" >
-    <meta name="description" content="Art, Techology & Music Experience AHEAD of its Time. 20-29 December 2019 at The Tribrata Darmawangsa, South Jakarta.">
+    <meta name="description" content="20-29 December 2019 at The Tribrata Darmawangsa, South Jakarta. Art, Techology & Music Experience AHEAD of its Time. ">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
     <meta name="robots" content="index, follow" >
     <meta name="language" content="English" >
     <meta name="apple-mobile-web-app-capable" content="yes" >
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
-    <meta name="keywords" content="Art, Techology, Music Experience, acara musik, acara musik desember 2019, acara musik tribata darmawangsa 2019, wot 2019, wave of tomorrow.id, wave of tomorrow, wave of tomorrow indonesia, wot indonesia, waveoftomorrow, wave of tomorrow id">
+    <meta name="keywords" content="Art, Techology, Music Experience, tribata darmawangsa 2019, wot 2019, wave of tomorrow.id, wave of tomorrow, wave of tomorrow, wot, waveoftomorrow, wave of tomorrow id">
     <link href="{{ asset('apple-icon-57x57.png') }}" rel="apple-touch-icon" sizes="57x57">
     <link href="{{ asset('apple-icon-60x60.png') }}" rel="apple-touch-icon" sizes="60x60">
     <link href="{{ asset('apple-icon-72x72.png') }}" rel="apple-touch-icon" sizes="72x72">
